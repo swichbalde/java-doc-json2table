2e07cdc480ce98d95c21c563988b07300d4f219a
swichbalde <lisnih55@gmail.com>
2022-09-13 16:40:43 +0300

    first version of java doc(json2table)

---
+++ src/main/java/com/nix/App.java
@@ -0,0 +1,58 @@
+package com.nix;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> keys = new ArrayList<>();
+        Map<String, Object> jsonElements = new ObjectMapper().readValue(new File("src/main/resources/request.json"), new TypeReference<>() {
+        });
+
+        Map<String, Object> values = new LinkedHashMap<>();
+        Map<String, String> types = new LinkedHashMap<>();
+
+        getAllKeys(jsonElements, keys, values, types);
+
+        for (String key : keys) {
+            System.out.println(key + " , " + types.get(key) + " , " + values.get(key));
+        }
+    }
+
+    private static void getAllKeys(Map<String, Object> jsonElements, List<String> keys, Map<String, Object> values, Map<String, String> types) {
+
+        jsonElements.forEach((key, value) -> {
+            keys.add(key);
+            if (!(value instanceof List)) {
+                values.put(key, value);
+                if (value == null) {
+                    types.put(key, "null");
+                } else {
+                    types.put(key, String.valueOf(value.getClass()));
+                }
+            }
+            if (value instanceof Map) {
+                Map<String, Object> map = (Map<String, Object>) value;
+                getAllKeys(map, keys, values, types);
+            } else if (value instanceof List) {
+                List<?> list = (List<?>) value;
+                list.forEach(listEntry -> {
+                    if (listEntry instanceof Map) {
+                        Map<String, Object> map = (Map<String, Object>) listEntry;
+                        getAllKeys(map, keys, values, types);
+                    }
+                });
+            }
+        });
+    }
+
+}
